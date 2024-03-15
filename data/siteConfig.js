00af1004278cacf2fc6336256791fda335a6e704
Ward Stinissen <wstini@Wards-MacBook-Pro.local>
2024-03-15 10:54:13 +0100

    Add Nike and finish Nexuzhealth

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,127 +1,137 @@
 module.exports = {
-  siteTitle: 'Ward Stinissen',
-  siteDescription: `Ward Stinissen Software Developer`,
-  authorName: 'Ward Stinissen',
-  twitterUsername: 'wardstinissen',
-  githubUsername: 'WardStinissen',
-  authorAvatar: '/images/avatar.jpg',
-  authorDescription: `Developer, passionate about what I do. Always interested in everything about technology and developement. <br />In 2013 I started studying applied computer science at Hogeschool PXL.<br />In 2016, I started working as a Java software crafter/Consulting at Continuum Consulting.<br />I currently work as a backend developer with a great interest in frontend and I mainly work with <strong>Java, Spring</strong> and <strong>Angular</strong>.`,
-  skills: [
-    {
-      name: 'Java',
-      level: 70
-    }, {
-      name: 'Spring / Spring Boot',
-      level: 60
-    }, {
-      name: 'Golang',
-      level: 50
-    }, {	
-      name: 'JavaScript',	
-      level: 50	
-    }, {	
-      name: 'HTML',	
-      level: 60	
-    }, {	
-      name: 'CSS',	
-      level: 50
-    }, {
-      name: 'Angular',
-      level: 60
-    }, {
-      name: 'Git',
-      level: 70
+    siteTitle: 'Ward Stinissen',
+    siteDescription: `Ward Stinissen Software Developer`,
+    authorName: 'Ward Stinissen',
+    twitterUsername: 'wardstinissen',
+    githubUsername: 'WardStinissen',
+    authorAvatar: '/images/avatar.jpg',
+    authorDescription: `Developer, passionate about what I do. Always interested in everything about technology and developement. <br />In 2013 I started studying applied computer science at Hogeschool PXL.<br />In 2016, I started working as a Java software crafter/Consulting at Continuum Consulting.<br />I currently work as a backend developer with a great interest in frontend and I mainly work with <strong>Java, Spring</strong> and <strong>Angular</strong>.`,
+    skills: [
+        {
+            name: 'Java',
+            level: 70
+        }, {
+            name: 'Spring / Spring Boot',
+            level: 60
+        }, {
+            name: 'Golang',
+            level: 80
+        }, {
+            name: 'JavaScript',
+            level: 50
+        }, {
+            name: 'HTML',
+            level: 60
+        }, {
+            name: 'CSS',
+            level: 50
+        }, {
+            name: 'Angular',
+            level: 60
+        }, {
+            name: 'Git',
+            level: 70
+        },
+    ],
+    jobs: [
+        {
+            company: "Nike",
+            begin: {
+                month: 'jan',
+                year: '2023'
+            },
+            duration: null,
+            occupation: "Software developer",
+            description: "Part of the customs team working on the customs inventory applications. Technologies: Java 17, Quarkus, AWS, Serverless, Spring, Scrum, Agile"
+        },
+        {
+            company: "Nexuzhealth",
+            begin: {
+                month: 'jul',
+                year: '2019'
+            },
+            duration: '3 year and 6 months',
+            occupation: "Software developer",
+            description: "Frontend and backend development of a elektronic medical file focussing on general practicioners and homenursing. Technologies: Golang, Angular, Google Cloud Spanner, gRPC, Scrum, Agile"
+        },
+        {
+            company: "Ravago",
+            begin: {
+                month: 'jun',
+                year: '2017'
+            },
+            duration: '2 year and 2 months',
+            occupation: "Software developer",
+            description: "Development and maintenance of a self written ERP system that is used world wide by all the companies in the Ravago group. Externalized document generation to lower the load on the system. Created a smart search application to search information on companies. Technologies: Java 7, Java 8, Spring, Spring Boot 1+2, Angular, Selenium, JBoss, Kubernetes, Docker, Mockito, DB2, Cassandra, Lucene plugin, Intellij, Confluence, Jira, Git, Bamboo, JSP, Scrum/Agile, ActiveMQ, IBMMQ"
+        }, {
+            company: "J&J",
+            begin: {
+                month: 'apr',
+                year: '2017'
+            },
+            duration: '3 months',
+            occupation: "Software developer",
+            description: "Development and maintenance of multiple applications. Technologies: Java 8, Spring boot, AngularJS"
+        }, {
+            company: "Axygest",
+            begin: {
+                month: 'sep',
+                year: '2016'
+            },
+            duration: '11 months',
+            occupation: "Software developer",
+            description: "Frontend and backend developement of an admin pannel for an organisation who manages payments of Taxi companies. Technologies: Java 8, Spring boot, AngularJS, Jenkins"
+
+        }, {
+            company: "Continuum Consulting NV",
+            begin: {
+                month: 'aug',
+                year: '2016'
+            },
+            duration: null,
+            occupation: "Java Software Crafter",
+            description: "I work as a consultant on multiple projects for different customers. I mostly do Java development but also help out in the frontend (Angular)."
+
+        }, {
+            company: "Continuum Consulting NV",
+            begin: {
+                month: 'feb',
+                year: '2016'
+            },
+            duration: '5 months',
+            occupation: "Internship",
+            description: "Worked as an intern. Did research on an internal API and how to expose parts of it externally."
+
+        }, {
+            company: "Hogeschool PXL",
+            begin: {
+                month: 'sep',
+                year: '2013'
+            },
+            duration: '3 years',
+            occupation: "Applied computer science",
+            description: "Specialization: Software Management"
+
+        },
+    ],
+    social: {
+        twitter: "https://twitter.com/wardstinissen",
+        linkedin: "https://www.linkedin.com/in/wardstinissen",
+        github: "https://github.com/WardStinissen",
+        email: "ward.stinissen+website@gmail.com"
     },
-  ],
-  jobs: [
-    {
-      company: "Nexuzhealth",
-      begin: {
-        month: 'jul',
-        year: '2019'
-      },
-      duration: null,
-      occupation: "Software developer",
-      description: "Frontend and backend development of a elektronic medical file focussing on general practicioners and homenursing. Technologies: Golang, Angular, Google Cloud Spanner, gRPC, Scrum, Agile"
-    },
-    {
-      company: "Ravago",
-      begin: {
-        month: 'jun',
-        year: '2017'
-      },
-      duration: '2 year and 2 months',
-      occupation: "Software developer",
-      description: "Development and maintenance of a self written ERP system that is used world wide by all the companies in the Ravago group. Externalized document generation to lower the load on the system. Created a smart search application to search information on companies. Technologies: Java 7, Java 8, Spring, Spring Boot 1+2, Angular, Selenium, JBoss, Kubernetes, Docker, Mockito, DB2, Cassandra, Lucene plugin, Intellij, Confluence, Jira, Git, Bamboo, JSP, Scrum/Agile, ActiveMQ, IBMMQ"
-    },  {
-      company: "J&J",
-      begin: {
-        month: 'apr',
-        year: '2017'
-      },
-      duration: '3 months',
-      occupation: "Software developer",
-      description: "Development and maintenance of multiple applications. Technologies: Java 8, Spring boot, AngularJS"
-    }, {
-      company: "Axygest",
-      begin: {
-        month: 'sep',
-        year: '2016'
-      },
-      duration: '11 months',
-      occupation: "Software developer",
-      description: "Frontend and backend developement of an admin pannel for an organisation who manages payments of Taxi companies. Technologies: Java 8, Spring boot, AngularJS, Jenkins"
-  
-    }, {
-      company: "Continuum Consulting NV",
-      begin: {
-        month: 'aug',
-        year: '2016'
-      },
-      duration: null,
-      occupation: "Java Software Crafter",
-      description: "I work as a consultant on multiple projects for different customers. I mostly do Java development but also help out in the frontend (Angular)."
-  
-    }, {
-      company: "Continuum Consulting NV",
-      begin: {
-        month: 'feb',
-        year: '2016'
-      },
-      duration: '5 months',
-      occupation: "Internship",
-      description: "Worked as an intern. Did research on an internal API and how to expose parts of it externally."
-  
-    }, {
-      company: "Hogeschool PXL",
-      begin: {
-        month: 'sep',
-        year: '2013'
-      },
-      duration: '3 years',
-      occupation: "Applied computer science",
-      description: "Specialization: Software Management"
-  
-    },
-  ],
-  social: {
-    twitter: "https://twitter.com/wardstinissen",
-    linkedin: "https://www.linkedin.com/in/wardstinissen",
-    github: "https://github.com/WardStinissen",
-    email: "ward.stinissen+website@gmail.com"
-  },
-  siteUrl: 'https://wardstinissen.be',
-  pathPrefix: '/website-ward-stinissen', // Note: it must *not* have a trailing slash.
-  siteCover: '/images/laptop-close.jpg',
-  googleAnalyticsId: 'UA-132465470-1',
-  background_color: '#ffffff',
-  theme_color: '#25303B',
-  display: 'minimal-ui',
-  icon: 'static/images/avatar.jpg',
-  headerLinks: [
-    {
-      label: 'Ward Stinissen',
-      url: '/',
-    }
-  ]
+    siteUrl: 'https://wardstinissen.be',
+    pathPrefix: '/website-ward-stinissen', // Note: it must *not* have a trailing slash.
+    siteCover: '/images/laptop-close.jpg',
+    googleAnalyticsId: 'UA-132465470-1',
+    background_color: '#ffffff',
+    theme_color: '#25303B',
+    display: 'minimal-ui',
+    icon: 'static/images/avatar.jpg',
+    headerLinks: [
+        {
+            label: 'Ward Stinissen',
+            url: '/',
+        }
+    ]
 }
